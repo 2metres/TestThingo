7a8b8686be64033fea4c9530dc60e7c44c2df9c2
atomist <admin@atomist.com>
2016-08-31 17:56:28 -0700

    Initial commit from the atomist bot

---
+++ src/main/java/com/myorg/ClassNameApplication.java
@@ -0,0 +1,12 @@
+package com.myorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassNameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassNameApplication.class, args);
+    }
+}
